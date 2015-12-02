d1aff48ab454dc6f08532cb9e7b042eb0cdfb537
Beck Chen <bcfchen@gmail.com>
2015-12-02 00:09:34 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+RARE service
+==========================
+
+NodeJS service for processing payments
+
+##Requirements
+* [node.js](http://nodejs.org)
+
+### Getting started
+
+####I. install
+    $ cd nailArtist-node
+    $ npm install
+
+####II. start
+    $ npm start
+
+#### Notes
+Service will be running at port `3006`
---
+++ app.js
@@ -0,0 +1,37 @@
+var express  = require('express'),
+		app  = express(),
+    server   = require('http').createServer(app),
+     stripe = require('stripe')('sk_test_mJmcYp1rfAiLqgi09pmWpoVT'),
+		bodyParser = require('body-parser');
+app.use(bodyParser.json({ type: 'application/json' }));
+
+app.set('port', 3006);
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.header("Access-Control-Allow-Origin", "*");
+	next();
+});
+
+app.post('/charge', function(req, res){
+ var stripeToken = req.body.id;
+    var amount = 1000;
+
+    stripe.charges.create({
+        card: stripeToken,
+        currency: 'usd',
+        amount: amount
+    },
+    function(err, charge) {
+        if (err) {
+            res.send(500, err);
+        } else {
+            console.log("charge", charge);
+            res.send(200, charge);
+        }
+    });
+});
+
+server.listen(app.get('port'), function () {
+  require(__dirname + '/document')(app._router.stack);
+  console.log('NailArtist payment service listening on port ' + server.address().port);
+});
