72ed63eacfc528ceba66dc96affb24ac83c9c810
Beck Chen <bcfchen@gmail.com>
2015-12-07 20:44:35 -0800

    modify to use new key

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express  = require('express'),
 		app  = express(),
     server   = require('http').createServer(app),
-     stripe = require('stripe')('sk_test_mJmcYp1rfAiLqgi09pmWpoVT'),
+     stripe = require('stripe')('sk_test_ZRz70EBxStjlGr9qqEF7NgWu'),
 		bodyParser = require('body-parser');
 app.use(bodyParser.json({ type: 'application/json' }));
 
