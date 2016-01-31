9f3653eff9a37c88b8f304c35d734711bfd32564
Beck Chen <bcfchen@gmail.com>
2016-01-31 14:40:28 -0800

    [Beck] add endpoint for retrieving stripe customer

--- app.js
+++ app.js
@@ -1,47 +1,89 @@
-var express  = require('express'),
-		app  = express(),
-    server   = require('http').createServer(app),
-     stripe = require('stripe')('sk_live_pIpwOXMlPSL8CbPDdlxLDS3U'),
-		bodyParser = require('body-parser');
-app.use(bodyParser.json({ type: 'application/json' }));
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    stripe = require('stripe')('sk_test_ZRz70EBxStjlGr9qqEF7NgWu'),
+    bodyParser = require('body-parser');
+app.use(bodyParser.json({
+    type: 'application/json'
+}));
 
 app.set('port', process.env.PORT || 3006);
 app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.header("Access-Control-Allow-Origin", "*");
-	next();
+    next();
 });
 
-app.post('/charge', function(req, res){
- var card = req.body.card;
- var amount = req.body.amount;
- var stripeToken = card.id;
+app.get('/charge', function(req, res) {
+    var card = req.body.card;
+    var amount = req.body.amount;
+    var stripeToken = card.id;
 
-console.log("card ", card);
-console.log("amount ", amount);
+    console.log("card ", card);
+    console.log("amount ", amount);
 
     stripe.charges.create({
-        card: stripeToken,
-        currency: 'usd',
-        amount: amount,
-        receipt_email: card.email
-    },
-    function(err, charge) {
-        if (err) {
-            res.send(500, err);
-        } else {
-            console.log("charge", charge);
-            res.send(200, charge);
-        }
-    });
+            card: stripeToken,
+            currency: 'usd',
+            amount: amount,
+            receipt_email: card.email
+        },
+        function(err, charge) {
+            if (err) {
+                res.send(500, err);
+            } else {
+                console.log("charge", charge);
+                res.send(200, charge);
+            }
+        });
 });
 
-app.get('/status', function(req, res){
+app.post('/charge', function(req, res) {
+    var card = req.body.card;
+    var amount = req.body.amount;
+    var stripeToken = card.id;
+
+    console.log("card ", card);
+    console.log("amount ", amount);
+
+    stripe.charges.create({
+            card: stripeToken,
+            currency: 'usd',
+            amount: amount,
+            receipt_email: card.email
+        },
+        function(err, charge) {
+            if (err) {
+                res.send(500, err);
+            } else {
+                console.log("charge", charge);
+                res.send(200, charge);
+            }
+        });
+});
+
+app.get('/getCustomer/:customerId', function(req, res) {
+    try {
+        var customerId = req.params && req.params.customerId ? req.params.customerId : undefined;
+        if (customerId) {
+            stripe.customers.retrieve(
+                "customerId",
+                function(err, customer) {
+                    res.send(200, customer);
+                }
+            );
+        }
+    } catch (e) {
+        res.status(404).end();
+    }
+});
+
+app.get('/status', function(req, res) {
     var status = "All Good";
     res.send(200, status);
 });
 
-server.listen(app.get('port'), function () {
-  require(__dirname + '/document')(app._router.stack);
-  console.log('NailArtist payment service listening on port ' + server.address().port);
+server.listen(app.get('port'), function() {
+    require(__dirname + '/document')(app._router.stack);
+    console.log('NailArtist payment service listening on port ' + server.address().port);
 });
